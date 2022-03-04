cd718bc60df16dc57ce69c7c93737984e8172897
tandejun <dejun.18@ichat.sp.edu.sg>
2022-03-04 18:35:13 +0800

    added access token
    modify geocoder lib for it to work

--- ip_process.py
+++ ip_process.py
@@ -3,4 +3,6 @@ import geocoder
 
 def geolocation(ip):
     g = geocoder.ip(ip)
+    # Uses access token, increases rate limit to 50k/month
+    #g = geocoder.ip(ip, token = '238eca1d31ff39')
     return {"lat": g.lat, "lng": g.lng, "country":g.country, "hostname":g.hostname, "org":g.org, "city":g.city}
